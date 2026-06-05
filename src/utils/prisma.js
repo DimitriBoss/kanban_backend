f20c50d25c5af2ad9c151d353571f5512a4d4fbc
DimitriBoss <dimitricrepin1@gmail.com>
2026-06-05 02:20:47 +0100

    fix_prisma

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import prismaClientPkg from "@prisma/client";
+const { PrismaClient } = prismaClientPkg;
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
