2f3ecb452205354baf7c365a155036a5a0688cad
DimitriBoss <dimitricrepin1@gmail.com>
2026-06-10 17:26:42 +0100

    autorisation cors

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import routesV2 from "./routes/routes.v2.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-
-// {
-//     origin: ["https://kanban-two-eta.vercel.app"],
-//     credentials: true,
-//   }
+app.use(
+  cors({
+    origin: ["https://kanban-two-eta.vercel.app"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from kanban");
